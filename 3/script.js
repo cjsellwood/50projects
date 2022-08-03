aa880553a34b041682d2e5f49fec6bf9ffb1265d
cjsellwood <c.sellwood22@gmail.com>
2022-08-03 14:46:29 +1000

    3. Styled rotate button

--- 3/script.js
+++ 3/script.js
@@ -7,12 +7,14 @@ rotateBtn.addEventListener("click", () => {
   if (!menuOpen) {
     main.style.transform = "rotate(-20deg)";
     navMenu.classList.remove("open");
-    navMenu.style.transform = "translateX(0px)"
+    navMenu.style.transform = "translateX(0px)";
+    rotateBtn.style.transform = "rotate(-90deg)";
     menuOpen = true;
   } else {
     main.style.transform = "rotate(0deg)";
     navMenu.classList.add("open");
-    navMenu.style.transform = "translateX(-200px)"
+    navMenu.style.transform = "translateX(-200px)";
+    rotateBtn.style.transform = "rotate(0)";
     menuOpen = false;
   }
 });
