a90f808c540af64a6a6f3df4bd40c131a575f7b6
cjsellwood <c.sellwood22@gmail.com>
2022-08-06 09:54:57 +1000

    6. Scroll animation complete

---
+++ 6/script.js
@@ -0,0 +1,21 @@
+// Move content box onto screen if bottom is in window
+function moveContent() {
+  const windowHeight = window.innerHeight;
+  for (let i = 0; i < contentBoxes.length; i++) {
+    const boxBottomPosition =
+    contentBoxes[i].offsetTop + contentBoxes[i].clientHeight;
+    if (boxBottomPosition < windowHeight + scrollY) {
+      contentBoxes[i].classList.add("center");
+    } else {
+      contentBoxes[i].classList.remove("center");
+    }
+  }
+}
+
+const contentBoxes = document.querySelectorAll(".content");
+
+moveContent();
+
+window.addEventListener("scroll", () => {
+  moveContent();
+});
