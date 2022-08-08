95d4b600c9ab4533e728872dce6129aac7cddb1f
cjsellwood <c.sellwood22@gmail.com>
2022-08-08 10:14:17 +1000

    8. Form Input Wave complete

--- 8/script.js
+++ 8/script.js
@@ -3,23 +3,41 @@ const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
 const emailLabel = document.getElementById("email-label");
 const passwordLabel = document.getElementById("password-label");
+const emailLetters = emailLabel.querySelectorAll("span");
+const passwordLetters = passwordLabel.querySelectorAll("span");
 emailInput.addEventListener("focusin", () => {
     emailInput.classList.add("typing");
     emailLabel.classList.add("typing");
+    emailLetters.forEach((letter, i) => {
+        letter.style.transform = "translateY(-24px)";
+        letter.style.transitionDelay = `${i / 20}s`;
+    });
 });
 emailInput.addEventListener("focusout", () => {
     if (emailInput.value === "") {
         emailInput.classList.remove("typing");
         emailLabel.classList.remove("typing");
+        emailLetters.forEach((letter, i) => {
+            letter.style.transform = "translateY(0px)";
+            letter.style.transitionDelay = `${i / 20}s`;
+        });
     }
 });
 passwordInput.addEventListener("focusin", () => {
     passwordInput.classList.add("typing");
     passwordLabel.classList.add("typing");
+    passwordLetters.forEach((letter, i) => {
+        letter.style.transform = "translateY(-24px)";
+        letter.style.transitionDelay = `${i / 20}s`;
+    });
 });
 passwordInput.addEventListener("focusout", () => {
     if (passwordInput.value === "") {
         passwordInput.classList.remove("typing");
         passwordLabel.classList.remove("typing");
+        passwordLetters.forEach((letter, i) => {
+            letter.style.transform = "translateY(0px)";
+            letter.style.transitionDelay = `${i / 20}s`;
+        });
     }
 });
--- 8/script.ts
+++ 8/script.ts
@@ -4,27 +4,49 @@ const emailLabel = document.getElementById("email-label")! as HTMLLabelElement;
 const passwordLabel = document.getElementById(
   "password-label"
 )! as HTMLLabelElement;
+const emailLetters = emailLabel.querySelectorAll(
+  "span"
+)! as NodeListOf<HTMLSpanElement>;
+const passwordLetters = passwordLabel.querySelectorAll(
+  "span"
+)! as NodeListOf<HTMLSpanElement>;
 
 emailInput.addEventListener("focusin", () => {
   emailInput.classList.add("typing");
   emailLabel.classList.add("typing");
+  emailLetters.forEach((letter, i) => {
+    letter.style.transform = "translateY(-24px)";
+    letter.style.transitionDelay = `${i / 20}s`;
+  });
 });
 
 emailInput.addEventListener("focusout", () => {
   if (emailInput.value === "") {
     emailInput.classList.remove("typing");
     emailLabel.classList.remove("typing");
+    emailLetters.forEach((letter, i) => {
+      letter.style.transform = "translateY(0px)";
+      letter.style.transitionDelay = `${i / 20}s`;
+    });
   }
 });
 
 passwordInput.addEventListener("focusin", () => {
   passwordInput.classList.add("typing");
   passwordLabel.classList.add("typing");
+  passwordLetters.forEach((letter, i) => {
+    letter.style.transform = "translateY(-24px)";
+    letter.style.transitionDelay = `${i / 20}s`;
+  });
 });
 
 passwordInput.addEventListener("focusout", () => {
   if (passwordInput.value === "") {
     passwordInput.classList.remove("typing");
     passwordLabel.classList.remove("typing");
+    passwordLetters.forEach((letter, i) => {
+      letter.style.transform = "translateY(0px)";
+      letter.style.transitionDelay = `${i / 20}s`;
+    });
   }
 });
