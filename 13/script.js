e75be3a342324baf04553870efc740337c101772
cjsellwood <c.sellwood22@gmail.com>
2022-08-13 09:57:52 +1000

    13. Random choice picker complete

--- 13/script.js
+++ 13/script.js
@@ -1,1 +1,38 @@
 "use strict";
+const choiceTextarea = document.getElementById("choice-textarea");
+const choicesContainer = document.getElementById("choices-container");
+let choices = [];
+choiceTextarea.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        pickChoice();
+        choiceTextarea.value = "";
+        return;
+    }
+    addChoices();
+});
+function addChoices() {
+    choicesContainer.replaceChildren();
+    // Remove values with no value or only spaces
+    choices = choiceTextarea.value.split(",").filter((x) => x.trim() !== "");
+    console.log(choices);
+    choices.forEach((choice) => {
+        const choiceElement = document.createElement("p");
+        choiceElement.textContent = choice;
+        choicesContainer.append(choiceElement);
+    });
+}
+function pickChoice() {
+    const choiceElements = choicesContainer.querySelectorAll("p");
+    let i = 0;
+    const interval = setInterval(() => {
+        choiceElements.forEach((el) => {
+            el.classList.remove("chosen");
+        });
+        const random = Math.floor(Math.random() * choiceElements.length);
+        choiceElements[random].classList.add("chosen");
+        i++;
+        if (i >= 30) {
+            clearInterval(interval);
+        }
+    }, 100);
+}
---
+++ 13/script.ts
@@ -0,0 +1,46 @@
+const choiceTextarea = document.getElementById(
+  "choice-textarea"
+)! as HTMLTextAreaElement;
+const choicesContainer = document.getElementById(
+  "choices-container"
+)! as HTMLDivElement;
+let choices = [];
+
+choiceTextarea.addEventListener("keyup", (e) => {
+  if (e.key === "Enter") {
+    pickChoice();
+    choiceTextarea.value = "";
+    return;
+  }
+  addChoices();
+});
+
+function addChoices() {
+  choicesContainer.replaceChildren();
+  // Remove values with no value or only spaces
+  choices = choiceTextarea.value.split(",").filter((x) => x.trim() !== "");
+  console.log(choices);
+  choices.forEach((choice) => {
+    const choiceElement = document.createElement("p");
+    choiceElement.textContent = choice;
+    choicesContainer.append(choiceElement);
+  });
+}
+
+function pickChoice() {
+  const choiceElements = choicesContainer.querySelectorAll(
+    "p"
+  )! as NodeListOf<HTMLParagraphElement>;
+  let i = 0;
+  const interval = setInterval(() => {
+    choiceElements.forEach((el) => {
+      el.classList.remove("chosen");
+    });
+    const random = Math.floor(Math.random() * choiceElements.length);
+    choiceElements[random].classList.add("chosen");
+    i++;
+    if (i >= 30) {
+      clearInterval(interval)
+    }
+  }, 100);
+}
