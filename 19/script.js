100b35891d848983cc9baadb2083185b2e868227
cjsellwood <c.sellwood22@gmail.com>
2022-08-19 10:25:11 +1000

    19. Theme Clock complete

--- 19/script.js
+++ 19/script.js
@@ -14,3 +14,73 @@ const toggleTheme = () => {
     }
 };
 themeBtn.addEventListener("click", toggleTheme);
+const secondHand = document.getElementById("second-hand");
+const minuteHand = document.getElementById("minute-hand");
+const hourHand = document.getElementById("hour-hand");
+const time = document.getElementById("time");
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const startClock = () => {
+    setHands();
+    setTime();
+    setDate();
+    setDay();
+    setInterval(() => {
+        setHands();
+        setTime();
+        setDay();
+        setDate();
+    }, 1000);
+};
+const setHands = () => {
+    const dateNow = new Date();
+    const hours = dateNow.getHours();
+    const minutes = dateNow.getMinutes();
+    hourHand.style.transform = `rotate(${((hours + minutes / 60) / 12) * 360 - 90}deg)`;
+    minuteHand.style.transform = `rotate(${(minutes / 60) * 360 - 90}deg)`;
+    const seconds = dateNow.getSeconds();
+    secondHand.style.transform = `rotate(${(seconds / 60) * 360 - 90}deg )`;
+};
+const setTime = () => {
+    const dateNow = new Date();
+    const timeNow = new Intl.DateTimeFormat("en-US", {
+        minute: "2-digit",
+        hour: "numeric",
+    }).format(dateNow);
+    time.textContent = timeNow;
+};
+const setDate = () => {
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const months = [
+        "JAN",
+        "FEB",
+        "MAR",
+        "APR",
+        "MAY",
+        "JUN",
+        "JUL",
+        "AUG",
+        "SEP",
+        "OCT",
+        "NOV",
+        "DEC",
+    ];
+    const dateNow = new Date();
+    const dayNumber = dateNow.getDay();
+    const monthNumber = dateNow.getMonth();
+    date.textContent = `${days[dayNumber]}, ${months[monthNumber]}`;
+};
+const setDay = () => {
+    const dateNow = new Date();
+    const dayNumber = dateNow.getDate();
+    day.textContent = dayNumber.toString();
+};
+window.addEventListener("load", startClock);
--- 19/script.ts
+++ 19/script.ts
@@ -14,3 +14,89 @@ const toggleTheme = () => {
 };
 
 themeBtn.addEventListener("click", toggleTheme);
+
+const secondHand = document.getElementById("second-hand")! as HTMLDivElement;
+const minuteHand = document.getElementById("minute-hand")! as HTMLDivElement;
+const hourHand = document.getElementById("hour-hand")! as HTMLDivElement;
+
+const time = document.getElementById("time")! as HTMLHeadingElement;
+const date = document.getElementById("date")! as HTMLParagraphElement;
+const day = document.getElementById("day")! as HTMLSpanElement;
+
+const startClock = () => {
+  setHands();
+  setTime();
+  setDate();
+  setDay();
+
+  setInterval(() => {
+    setHands();
+    setTime();
+    setDay();
+    setDate();
+  }, 1000);
+};
+
+const setHands = () => {
+  const dateNow = new Date();
+  const hours = dateNow.getHours();
+  const minutes = dateNow.getMinutes();
+  hourHand.style.transform = `rotate(${
+    ((hours + minutes / 60) / 12) * 360 - 90
+  }deg)`;
+
+  minuteHand.style.transform = `rotate(${(minutes / 60) * 360 - 90}deg)`;
+
+  const seconds = dateNow.getSeconds();
+  secondHand.style.transform = `rotate(${(seconds / 60) * 360 - 90}deg )`;
+};
+
+const setTime = () => {
+  const dateNow = new Date();
+  const timeNow = new Intl.DateTimeFormat("en-US", {
+    minute: "2-digit",
+    hour: "numeric",
+  }).format(dateNow);
+
+  time.textContent = timeNow;
+};
+
+const setDate = () => {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const months = [
+    "JAN",
+    "FEB",
+    "MAR",
+    "APR",
+    "MAY",
+    "JUN",
+    "JUL",
+    "AUG",
+    "SEP",
+    "OCT",
+    "NOV",
+    "DEC",
+  ];
+  const dateNow = new Date();
+  const dayNumber = dateNow.getDay();
+  const monthNumber = dateNow.getMonth();
+
+  date.textContent = `${days[dayNumber]}, ${months[monthNumber]}`;
+};
+
+const setDay = () => {
+  const dateNow = new Date();
+  const dayNumber = dateNow.getDate();
+  day.textContent = dayNumber.toString();
+};
+
+window.addEventListener("load", startClock);
