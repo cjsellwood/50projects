c480ab396569d1d7d768cd394cb66a6f42db341b
cjsellwood <c.sellwood22@gmail.com>
2022-08-20 10:14:41 +1000

    20. Button ripple effect complete

---
+++ 20/script.js
@@ -0,0 +1,15 @@
+"use strict";
+const button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+    let i = 0;
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const backgroundInterval = setInterval(() => {
+        button.style.backgroundImage = `radial-gradient(circle at ${x}px ${y}px, rgba(255, 255, 255, ${1 - i / 150}) ${i}%, purple ${i}%)`;
+        i += 2;
+        if (i > 150) {
+            button.style.backgroundImage = "none";
+            clearInterval(backgroundInterval);
+        }
+    }, 5);
+});
