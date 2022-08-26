501859a8e41aa48a9d2263e3fae58999e7468fbd
cjsellwood <c.sellwood22@gmail.com>
2022-08-26 13:57:26 +1000

    26. Vertical Slider complete

--- 26/script.js
+++ 26/script.js
@@ -2,15 +2,27 @@
 const downBtn = document.getElementById("down-arrow");
 const upBtn = document.getElementById("up-arrow");
 const leftShifter = document.getElementById("left-shifter");
+const rightShifter = document.getElementById("right-shifter");
 let leftHeight = 300;
+let rightHeight = 0;
 downBtn.addEventListener("click", () => {
     leftHeight = (leftHeight + 100) % 400;
     leftShifter.style.top = `-${leftHeight}vh`;
+    rightHeight = rightHeight + 100;
+    if (rightHeight > 0) {
+        rightHeight = -300;
+    }
+    rightShifter.style.top = `${rightHeight}vh`;
 });
 upBtn.addEventListener("click", () => {
-    leftHeight = (leftHeight - 100);
+    leftHeight = leftHeight - 100;
     if (leftHeight < 0) {
         leftHeight = 300;
     }
     leftShifter.style.top = `-${leftHeight}vh`;
+    rightHeight = rightHeight - 100;
+    if (rightHeight < -300) {
+        rightHeight = 0;
+    }
+    rightShifter.style.top = `${rightHeight}vh`;
 });
--- 26/script.ts
+++ 26/script.ts
@@ -1,18 +1,35 @@
 const downBtn = document.getElementById("down-arrow")! as HTMLButtonElement;
 const upBtn = document.getElementById("up-arrow")! as HTMLButtonElement;
+
 const leftShifter = document.getElementById("left-shifter")! as HTMLDivElement;
+const rightShifter = document.getElementById(
+  "right-shifter"
+)! as HTMLDivElement;
 
 let leftHeight = 300;
+let rightHeight = 0;
 
 downBtn.addEventListener("click", () => {
   leftHeight = (leftHeight + 100) % 400;
   leftShifter.style.top = `-${leftHeight}vh`;
+
+  rightHeight = rightHeight + 100;
+  if (rightHeight > 0) {
+    rightHeight = -300
+  }
+  rightShifter.style.top = `${rightHeight}vh`;
 });
 
 upBtn.addEventListener("click", () => {
-  leftHeight = (leftHeight - 100);
+  leftHeight = leftHeight - 100;
   if (leftHeight < 0) {
     leftHeight = 300;
   }
   leftShifter.style.top = `-${leftHeight}vh`;
+
+  rightHeight = rightHeight - 100;
+  if (rightHeight < -300) {
+    rightHeight = 0;
+  }
+  rightShifter.style.top = `${rightHeight}vh`;
 });
