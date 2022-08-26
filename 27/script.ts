8c2bd88cfcd6e6055225306190660ae6434b2fb5
cjsellwood <c.sellwood22@gmail.com>
2022-08-27 09:19:10 +1000

    27. Toast Notification complete

---
+++ 27/script.ts
@@ -0,0 +1,32 @@
+const notifyBtn = document.getElementById(
+  "notify-button"
+)! as HTMLButtonElement;
+const toastContainer = document.getElementById(
+  "toast-container"
+)! as HTMLDivElement;
+
+const randomNumber = () => {
+  const numbers = ["One", "Two", "Three", "Four"];
+  return numbers[Math.floor(Math.random() * 4)];
+};
+
+const randomColor = () => {
+  const colors = ["red", "green", "purple"];
+  return colors[Math.floor(Math.random() * 3)];
+};
+
+const addToast = () => {
+  const toastElement = document.createElement("div");
+  toastElement.classList.add("toast");
+
+  toastElement.textContent = `Message ${randomNumber()}`;
+  toastElement.style.color = randomColor();
+
+  toastContainer.append(toastElement);
+
+  setTimeout(() => {
+    toastElement.remove();
+  }, 3000);
+};
+
+notifyBtn.addEventListener("click", addToast);
